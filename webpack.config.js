faae94580714990d5177129bfc2442e41702f760
AndrewMik <andrew.mikitchuk@gmail.com>
2023-04-26 15:13:39 +0400

    init: start virtual-keyboard task

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+  },
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['js'] }) ];
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:jpg|jpeg|gif|png|ico|svg)$/i,
+        type: 'asset/resource', 
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource', 
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({ 
+      filename: '[name].[contenthash].css'
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false 
+    }),
+  ],
+  ...devServer(development)
+});
